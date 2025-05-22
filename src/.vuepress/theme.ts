f635cc7ccc7f19e1b3a399b5513b9c5ad51bdb5e
drizzle <msdrizzle@outlook.com>
2025-05-22 12:14:06 +0800

    refactor(theme): 注释掉默认的评论插件配置

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -53,13 +53,13 @@ export default hopeTheme({
 
   plugins: {
     // You should generate and use your own comment service
-    comment: {
-      provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
-      category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
-    },
+    // comment: {
+    //   provider: "Giscus",
+    //   repo: "vuepress-theme-hope/giscus-discussions",
+    //   repoId: "R_kgDOG_Pt2A",
+    //   category: "Announcements",
+    //   categoryId: "DIC_kwDOG_Pt2M4COD69",
+    // },
 
     components: {
       components: ["Badge", "VPCard"],
